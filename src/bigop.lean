3b883796cb41309c2a1549fdfa9be8503de472c8
Patrick Massot <patrickmassot@free.fr>
2018-05-15 09:58:08 +0200

    More natural numbers hell

--- src/bigop.lean
+++ src/bigop.lean
@@ -47,29 +47,30 @@ apply_bigop (*) 1 r (λ i, true) F
 /- variable is natural numbers from a to b filtered -/
 
 notation `big[`op`/`:0 nil`]_(`:0 binder`=`a`..`b `|` P:(scoped p, p)`)` F:(scoped f, f) := 
-apply_bigop op nil (range' a (b-a+1)) P F
+apply_bigop op nil (range' a (b+1-a)) P F
 
 notation `Σ_(`:0 binder`=`a`..`b `|` P:(scoped p, p)`)` F:(scoped f, f) := 
-apply_bigop (+) 0 (range' a (b-a+1)) P F
+apply_bigop (+) 0 (range' a (b+1-a)) P F
 
 notation `Π_(`:0 binder`=`a`..` b `|` P:(scoped p, p)`)` F:(scoped f, f) := 
-apply_bigop (*) 1 (range' a (b-a+1)) P F
+apply_bigop (*) 1 (range' a (b+1-a)) P F
 
 
 /- variable is natural numbers from a to b -/
 
 notation `big[`:0 op `/`:0 nil `]_(`:0 binder `=` a `..` b `)` F:(scoped f, f) := 
-apply_bigop op nil (range' a (b-a+1)) (λ i, true) F
+apply_bigop op nil (range' a (b+1-a)) (λ i, true) F
 
 notation `Σ_(`:0 binder `=` a `..` b `)` F:(scoped f, f) := 
-apply_bigop (+) 0 (range' a (b-a+1)) (λ i, true) F
+apply_bigop (+) 0 (range' a (b+1-a)) (λ i, true) F
 
 notation `Π_(`:0 binder `=` a `..` b `)` F:(scoped f, f) := 
-apply_bigop (*) 1 (range' a (b-a+1)) (λ i, true) F
+apply_bigop (*) 1 (range' a (b+1-a)) (λ i, true) F
 
 local notation `?(F` h`)` := if P h then F h else nil
 
 /- First lemmas, without assuming anything on `op` and `nil` -/
+
 
 lemma big.nil : (big[(◆)/nil]_(i ∈ [] | (P i)) (F i)) = nil :=
 by simp [apply_bigop]
@@ -81,6 +82,46 @@ by simp [apply_bigop, Ph]
 lemma big_cons_false {h} (t) (Ph : ¬ P h) :
   (big[(◆)/nil]_(i ∈ h::t | (P i)) (F i)) = (big[(◆)/nil]_(i ∈ t | (P i)) (F i)) :=
 by simp [apply_bigop, Ph]
+
+lemma filter_congr {α : Type*} {p q : α → Prop} [decidable_pred p] [decidable_pred q]
+  : ∀ {l : list α}, (∀ x ∈ l, p x ↔ q x) → filter p l = filter q l
+| [] _     := rfl
+| (a::l) h := by simp at h; by_cases pa : p a;
+  [simp [pa, h.1.1 pa, filter_congr h.2],
+   simp [pa, mt h.1.2 pa, filter_congr h.2]]
+
+lemma filter_ext {α : Type*} {r: list α} (P P') [decidable_pred P] [decidable_pred P'] 
+  (HP : ∀ i ∈ r, P i = P' i) : filter P r = filter P' r :=
+begin
+  induction r with h t IH,
+  { simp },
+  { have HPh : P h = P' h := HP h (by simp),
+    have : ∀ (i : α), i ∈ t → P i = P' i,
+    { intros i i_t,
+      exact (HP i $ by simp [i_t]) },
+    by_cases H : P h, 
+    { have H' : P' h := HPh ▸ H,
+      simp [H, H', IH this] },
+    { have H' : ¬ P' h := HPh ▸ H, 
+      simp [H, H', IH this] } }
+end
+
+lemma foldr_ext {α : Type*} {β : Type*} {l : list α} (f f' : α → β → β) (s : β)
+  (H : ∀ a ∈ l, ∀ b : β, f a b = f' a b) : foldr f s l = foldr f' s l :=
+by induction l; simp * {contextual := tt}
+
+-- A version of extensionality where we assume same (◆)/nil and same list
+@[extensionality]
+lemma big.ext (P') [decidable_pred P'] (F' : I → R) 
+  (HP : ∀ i ∈ r, P i ↔ P' i) (HF : ∀ i ∈ r, F i = F' i) :
+  (big[(◆)/nil]_(i ∈ r | (P i)) (F i)) = (big[(◆)/nil]_(i ∈ r | (P' i)) (F' i)) :=
+begin
+  unfold apply_bigop,
+  rw filter_congr HP,
+  apply foldr_ext,
+  intros _ i_r _,
+  simp[HF, mem_filter.1 i_r]
+end
 
 lemma range'_add_map (a b k : ℕ) : range' (a+k) b = map (λ x, x + k) (range' a b) :=
 begin
@@ -112,11 +153,21 @@ by simp[apply_bigop, filter_map_comm, foldr_map]
 
 --set_option pp.all true
 
+lemma big.empty_range (P : ℕ → Prop) [decidable_pred P] (F : ℕ → R) (a b : ℕ)
+  (H : b < a) : (big[(◆)/nil]_(i=a..b | (P i)) (F i)) = nil :=
+begin
+  have t : range' a (b + 1 - a) = list.nil, by simp [nat.sub_eq_zero_iff_le.2 H],
+  rw t,
+  apply big.nil op nil P F,
+end
+
 lemma big.shift (P : ℕ → Prop) [decidable_pred P] (F : ℕ → R) (a b k : ℕ) : 
   (big[(◆)/nil]_(i=a..b | (P i)) (F i)) = (big[(◆)/nil]_(i=(a+k)..(b+k) | (P (i-k))) (F (i-k))) :=
 begin
   rw [range'_add_map, big.map],
-  congr_n 1 ; funext ; simp only [nat.add_sub_cancel, nat.add_sub_add_right]
+  have : b + k + 1 - (a + k) = b + 1 - a := 
+    by rw [add_comm a, ← nat.sub_sub, add_right_comm, nat.add_sub_cancel],
+  congr_n 1 ; funext; simp only [nat.add_sub_cancel,  this]
 end
 
 /- Now we go towards assuming (R, op, nil) is a monoid -/
@@ -219,12 +270,16 @@ lemma big.gather_of_commute (F G : ℕ → R) (k l : ℕ)
   big[(◆)/nil]_(i = k..(k+l)) F i ◆ G i := 
 begin
   induction l with l IH,
-  { simp [big.one_term, nat.sub_self] },
+  { have : k + 1 - k = 1,
+    rw [add_comm, nat.add_sub_cancel],
+    simp [big.one_term, this] },
   { have obs : ∀ i, (i ∈ (range' k (l + 1))) → k + l + 1 ≠ i:=
       assume i h, ne_of_gt (mem_range'.1 h).2,
     
-    have : ∀ j, k + j - k + 1 = j + 1 :=
-      λ j, by conv in  (_ - _) {rw [add_comm, nat.add_sub_cancel]},
+    have : ∀ j, k + j + 1 - k = j +1 :=
+      λ j, calc 
+        k + j + 1 - k = k + (j + 1) - k : rfl
+                  ... = j+1 : by rw [add_comm, nat.add_sub_cancel],
     rw this at IH ⊢,
     rw [show range' k (nat.succ l + 1) = (range' k ( l + 1)) ++ [k+l+1],
         by simp [range'_concat]],
@@ -263,36 +318,44 @@ begin
   { simp [big.nil, Hnil] },
   { rw [big.cons, Hop, apply_ite _ _ Hnil, reverse_cons', big.append, IH, big.one_term'] }
 end
+variables (a b : nat)
+
+
+variables {α : Type*} {β : Type*}
+theorem nth_map (f : α → β) : ∀ l n, nth (map f l) n = (nth l n).map f
+| []       n     := rfl
+| (a :: l) 0     := rfl
+| (a :: l) (n+1) := nth_map l n
+
+theorem nth_le_map (f : α → β) {l n} (H1 H2) : nth_le (map f l) n H1 = f (nth_le l n H2) :=
+option.some.inj $ by rw [← nth_le_nth, nth_map, nth_le_nth]; refl
+
+#check nth_le_reverse
+
+#reduce let a:=5 in let b := 8 in map (λ i, 2 * a + (b + 1 - a) - i - 1) (range' a (b+1-a))
+#reduce let a:=5 in let b := 8 in map (λ i, a + b - i) (range' a (b+1-a))
 
 lemma reverse_range' (a b : ℕ) : reverse (range' a b) = map (λ i, 2*a+b-i-1) (range' a b) :=
 begin
-  induction b with b IH,
-  { simp },
-  { have pfff : (λ (i : ℕ), 2 * a + b - i - 1) ∘ (λ (x : ℕ), x - 1) = λ x, 2 * a + b + 1 - x - 1,
-    funext, simp, ring, sorry,
-    have bis : a + b = (λ (x : ℕ), 2 * a + b + 1 - x - 1) a,
-    simp, ring, sorry,
-    have ter : (a :: range' (a + 1) b : list ℕ) = range' a (b+1),
-    sorry,
-    have := calc
-    reverse (range' a (nat.succ b)) = reverse (range' a (b+1)) : rfl
-    ... = reverse ((range' a b) ++ [a+b]) : by rw range'_concat
-    ... = (a+b) :: reverse (range' a b) : reverse_append _ _
-    ... = (a + b) :: map (λ (i : ℕ), 2 * a + b - i - 1) (range' a b) : by rw IH
-    ... = (a + b) :: map (λ (i : ℕ), 2 * a + b - i - 1) (map (λ x, x-1) $ range' (a+1) b) : by rw range'_sub_map
-    ... = _ : by rw [map_map, pfff, bis, ←map_cons, ter],
-    exact this }
+  sorry
 end
 
-#check big.map
 lemma big.range_anti_mph {φ : R → R} (P : ℕ → Prop) [decidable_pred P] (F : ℕ → R) (a b : ℕ)
   (Hop : ∀ a b : R, φ (a ◆ b) = φ b ◆ φ a) (Hnil : φ nil = nil) :
-  φ (big[(◆)/nil]_(i=a..b | (P i)) F i) = big[(◆)/nil]_(i=a..b | (P (a-i+b))) φ (F (a-i+b)) := 
+  φ (big[(◆)/nil]_(i=a..b | (P i)) F i) = big[(◆)/nil]_(i=a..b | (P (a+b-i))) φ (F (a+b-i)) := 
 begin
-  rw big.anti_mph op nil _ _ _ Hop Hnil,
-  rw reverse_range',
-  rw big.map op nil _ (λ i, a-i+b) P,
-  dsimp [apply_bigop],
-  sorry
+  by_cases H : b < a,
+  { simp [big.empty_range, *] },
+  { rw big.anti_mph op nil _ _ _ Hop Hnil,
+    rw reverse_range',
+    rw big.map op nil _ _ _,
+    apply big.ext ; intros i i_in; have : 2 * a + (b + 1 - a) - i - 1 = a+b-i := 
+    begin
+      replace H : a ≤ b := le_of_not_gt H,
+      exact calc 2 * a + (b + 1 - a) - i - 1 = a + (a + (b + 1 - a)) - i - 1 : by simp [two_mul]
+        ... = a + (b + 1) - i - 1 : by rw nat.add_sub_of_le (le_trans H (nat.le_succ _))
+        ... = (a + b) + 1 - (i + 1)  : by simp [add_assoc,nat.sub_sub]
+        ... = a + b - i : by rw nat.add_sub_add_right,
+    end; rw this }
 end
 end monoid
